2568b493da885028bb39003394ec15a795460191
scottcain <scott@scottcain.net>
2009-04-21 17:52:06 +0000

    adding documentation
    git-svn-id: http://svn.dnalc.org/ga/pipeline@11 8e20c4c2-b435-4367-b5b9-847ebbf7797d

--- bin/create_user_environment.pl
+++ bin/create_user_environment.pl
@@ -7,17 +7,76 @@ use Bio::GMOD::DB::Config;
 use Getopt::Long;
 use Cwd;
 use File::Copy;
+use Pod::Usage;
 
-my ($USERNAME, $DUMPPATH, $DBPROF, $ORGSTRING,
-    $DATADIR,$GBROWSE_TEMPLATE,$GBROWSE_CONFDIR);
+=head1 NAME
 
+create_user_environment.pl - Do administrative tasks for setting up a new user
 
-=pod
+=head1 SYNOPSIS
 
-organism_string should be "genus_speceis_commonname" -- no need for quoting
-and can be split on '_'
+  % create_user_environment.pl --username <name> --organism_string <string> --data_dir <path>
+
+=head1 COMMAND-LINE OPTIONS
+
+=over
+
+=item username
+
+The name of the web-based user
+
+=item dumppath
+
+The path to the PostgreSQL dump file (default:/usr/local/gmod/src/chado_dump.bz2)
+
+=item profile
+
+The name of the GMOD conf file to use for database connection info (default:default)
+
+=item organism_string
+
+A string that has the genus, species and common name concatentated with underscores.
+Example: Test_testus_test or Homo_sapiens_human.
+
+=item data_dir
+
+The path to the directory where the user's GFF files.
+
+=item gbrowse_template
+
+The name of the template for creating GBrowse conf files (default:gbrowse.template)
+
+=item gbrowse_confdir
+
+The path to the GBrowse configuration file (default:/etc/httpd/conf/gbrowse.conf)
+
+=back
+
+=head1 DESCRIPTION
+
+This script accomplishes several tasks:
+  Creates a user-specific Chado database
+  Creates a GMOD conf file in $GMOD_ROOT/conf
+  Loads the Chado database with the user's analysis results
+  Creates a GBrowse conf file
+
+Soon (possibly): creates user specific Apollo conf stuff
+
+=head1 AUTHOR
+
+Scott Cain E<lt>cain.cshl@gmail.orgE<gt>
+
+Copyright (c) 2009
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =cut
+
+
+my ($HELP,$USERNAME, $DUMPPATH, $DBPROF, $ORGSTRING,
+    $DATADIR,$GBROWSE_TEMPLATE,$GBROWSE_CONFDIR);
+
 
 GetOptions(
   'username=s'         => \$USERNAME,
@@ -27,7 +86,8 @@ GetOptions(
   'data_dir=s'         => \$DATADIR,
   'gbrowse_template=s' => \$GBROWSE_TEMPLATE,
   'gbrowse_confdir=s'  =>  \$GBROWSE_CONFDIR,
-);
+) or pod2usage(-verbose => 1, -exitval => 1);
+
 
 die unless $USERNAME;
 
