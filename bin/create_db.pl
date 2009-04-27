e193e92a696d3bb150298c2cc9246dc73569b099
scottcain <scott@scottcain.net>
2009-04-27 14:27:28 +0000

    adding pod
    git-svn-id: http://svn.dnalc.org/ga/pipeline@20 8e20c4c2-b435-4367-b5b9-847ebbf7797d

--- bin/create_db.pl
+++ bin/create_db.pl
@@ -7,12 +7,55 @@ use DNALC::Pipeline::Chado::Utils;
 use Getopt::Long;
 use Pod::Usage;
 
+=head1 NAME
+
+create_db.pl - set up a new user's database
+
+=head1 SYNOPSIS
+
+  % create_db.pl --username <name>
+
+=head1 COMMAND-LINE OPTIONS
+
+=over
+
+=item username
+
+The name of the web-based user
+
+=item dumppath
+
+The path to bzip2 compressed PostgreSQL dump file (default:/usr/local/gmod/src/chado_dump.bz2)
+
+=item profile
+
+The name of the GMOD conf file to use for database connection info (default:default)
+
+=back
+
+=head1 DESCRIPTION
+
+This script takes command line arguemnts and creates a chado database
+poplulated with ontology terms but no features.
+
+=head1 AUTHOR
+
+Scott Cain E<lt>cain.cshl@gmail.orgE<gt>
+
+Copyright (c) 2009
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
 my ($PROFILE, $DUMPPATH, $USERNAME, $HELP);
 
 GetOptions(
   'username=s'         => \$USERNAME,
   'dumppath=s'         => \$DUMPPATH,
   'profile=s'          => \$PROFILE,
+  'help'               => \$HELP,
 ) or  pod2usage(-verbose => 1, -exitval => 1);
 
 pod2usage(-verbose => 2, -exitval => 1) if $HELP;
