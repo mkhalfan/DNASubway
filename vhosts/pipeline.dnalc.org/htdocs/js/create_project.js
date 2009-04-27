f83b2cb44c309664df6c9458790c65cbc821ab91
cghiban <cghiban@gmail.com>
2009-04-27 21:36:18 +0000

    - Web UI for creating projects, with file upload
    git-svn-id: http://svn.dnalc.org/ga/pipeline@26 8e20c4c2-b435-4367-b5b9-847ebbf7797d

--- vhosts/pipeline.dnalc.org/htdocs/js/create_project.js
+++ vhosts/pipeline.dnalc.org/htdocs/js/create_project.js
@@ -1,14 +1,38 @@
 function step_one() {
+	if (! $('seq_src_upload').checked && !$('seq_src_sample').checked) {
+		alert("Source not selected!");
+		return;
+	}
+
 	var f = $('forma1');
-	var has_file = $('seq_file').value != '';
-	var has_sample = $('specie').selectedIndex != '-1';
+	var has_file = $('seq_src_upload').checked && ( $('seq_file').value != '' );
+	var has_sample = $('seq_src_sample').checked && $('specie').selectedIndex != -1;
 	if (!has_file && !has_sample) {
 		alert("You must upload file or select a sample organism!");
 		return;
 	}
-	//alert(has_file + '-' + has_sample);
+	if (has_sample) {
+		alert("Method not yet supported!");
+		return;
+	}
 	f.submit();
 }
+
+function select_source(el) {
+	if (el && el.value == 'upload') {
+		$('specie').selectedIndex = -1;
+	}
+	else if (el && el.value == 'sample') {
+		$('organism_info').show();
+	}
+}
+
+function set_source(s) {
+	var el = $('seq_src_' + s);
+	if (el)
+		el.checked = true;
+}
+
 /*
 function step_one_jquery() {
 	var f = $("#forma1");
