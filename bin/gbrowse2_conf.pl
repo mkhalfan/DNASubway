23bee0c53b1a1b622020bccaddfcf97c00233c49
mkhalfan <mkhalfan@cshl.edu>
2011-06-16 16:05:38 +0000

    - preparing config files for GBrowse2
    git-svn-id: http://svn.dnalc.org/ga/pipeline@469 8e20c4c2-b435-4367-b5b9-847ebbf7797d

---
+++ bin/gbrowse2_conf.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl 
+
+use lib '/var/www/lib/perl';
+
+use common::sense;
+
+use DNALC::Pipeline::Config ();
+
+my $cf = DNALC::Pipeline::Config->new->cf('PIPELINE');
+
+my $username = $ARGV[0];
+my $pid = $ARGV[1];
+
+my $file = File::Spec->catfile($cf->{GBROWSE2_CONF_DIR}, sprintf("%s_db_%d.conf", $username, $pid));
+
+
+#print STDERR  $file, $/;
+#print $file, $/;
+
+open(FILE, $file) or die "Couldn't open $file: $!";
+while (<FILE>){
+	print $_;
+}
+close(FILE);
+
