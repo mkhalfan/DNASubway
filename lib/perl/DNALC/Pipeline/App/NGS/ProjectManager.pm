1e8f2e05c4c99af5efd181bb8c69c5cad26221e5
mkhalfan <mkhalfan@cshl.edu>
2011-11-18 15:03:52 +0000

    git-svn-id: http://svn.dnalc.org/ga/pipeline@510 8e20c4c2-b435-4367-b5b9-847ebbf7797d

--- lib/perl/DNALC/Pipeline/App/NGS/ProjectManager.pm
+++ lib/perl/DNALC/Pipeline/App/NGS/ProjectManager.pm
@@ -103,7 +103,7 @@ use iPlant::FoundationalAPI::Constants ':all';
 		my ($self, $app_name) = @_;
 
 		my $api_instance = $self->api_instance;
-		return {status => 'fail', msg => 'no api_instance object'} unless $api_instance;
+		return {status => 'fail', msg => 'sub app: no api_instance object'} unless $api_instance;
 
 		my $apps = $api_instance->apps;
 		my $app = $apps->find_by_name($app_name);
@@ -137,14 +137,43 @@ use iPlant::FoundationalAPI::Constants ':all';
 			#	}
 			#	print $/;
 			#}
-			return {status => 'success', inputs => $inputs, params => $params};
+
+			my $app_id = $app->[0]->id();
+			return {status => 'success', inputs => $inputs, params => $params, app_id => $app_id};
 
 		}
-
+		
 		else{
 			return {status => 'fail', msg => "App $app_name not found"};
 			print  STDERR "App $app_name not found sorry \n";
 		}
+
+	}
+	
+	#--------------------------------------
+	sub job {
+		my ($self, $app_id, $form_arguments) = @_;
+		
+		my $api_instance = $self->api_instance;
+		return {status => 'fail', msg => 'sub job: no api_instance object'} unless $api_instance;
+		
+		my %job_arguments = %$form_arguments;
+		my $app_id = $app_id;
+		my $apps = $api_instance->apps;
+		my $app = $apps->find_by_name($app_id);
+		
+		print "App ID: $app_id <br /> Annotation Field: $job_arguments{ANNOTATION} <br />";
+		
+		# adding additional 'hidden' arguments
+		$job_arguments{jobName} = $app_id . '-DNAS-' . int(rand(100));
+		$job_arguments{archive} = '1';
+		$job_arguments{processors} = '1';
+		$job_arguments{requested_time} = '11:11:11';
+		$job_arguments{softwareName} = $app_id;
+		
+		print Dumper (%job_arguments);
+		my $job_ep = $api_instance->job;
+		my $job = $job_ep->submit_job($app, %job_arguments);
 
 	}
 
