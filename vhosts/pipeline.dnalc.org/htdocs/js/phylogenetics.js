7020ce3b139e401b6aec47629ea3ae0ba9ebbdc2
mkhalfan <mkhalfan@cshl.edu>
2012-08-10 14:40:59 +0000

    the low QS alert should only appear when you rollover the alert icon, NOT the download trace file icon. this has been implemented in this fix
    git-svn-id: http://svn.dnalc.org/ga/pipeline@699 8e20c4c2-b435-4367-b5b9-847ebbf7797d

--- vhosts/pipeline.dnalc.org/htdocs/js/phylogenetics.js
+++ vhosts/pipeline.dnalc.org/htdocs/js/phylogenetics.js
@@ -2389,7 +2389,7 @@ Event.observe(window, Prototype.Browser.IE ? 'load' : 'dom:loaded', function() {
 	// step 22 is from the sequence viewer before you select a trace to view
 	else if (step == 22) {
 		// attach tool tip for low quality score alerts
-		$$('#mini-trace-icons div[id^=low]').each(function(el) {
+		$$('#mini-trace-icons span[id^=low]').each(function(el) {
 			var span_id = el.getAttribute('id');
 			debug("tip " + span_id);
 			new Tip(span_id, "The average error rate for this sequence is greater than 1%. This indicates that the sequence is of low quality and may produce erroneous analysis results.", {
